1a3654df641d9bcf6d2b888601a0075bc33bbcc4
ignacioeuy <ignacio.eat@gmail.com>
2022-11-23 08:20:52 -0300

    modifico statebucket

--- terraform/statebucket.tf
+++ terraform/statebucket.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "proyecto-ort-2022g2-tfstate"
     key = "main"
-    region + "us-east-1"
+    region = "us-east-1"
     dynamodb_table = "dynamo-state-table"
   }
 }
